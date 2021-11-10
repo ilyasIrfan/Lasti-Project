329d8bc911cfddc1a097ad83cf551182675de152
wpo9nine <wpo9nine@gmail.com>
2021-11-10 21:24:13 +0700

    add get makanan dan kondimen

--- qualityFood.py
+++ qualityFood.py
@@ -23,5 +23,33 @@ app = FastAPI()
 def root():
 	return ("Welcome to YamYam")
 
+@app.get("/kondimen")
+async def read_all_kondimens():
+	return data_bumbu
+
+@app.get("/makanan")
+async def read_all_food():
+	return data_menu
+
+@app.get("/kondimen/{kondimen_id}")
+async def read_kondimen(kondimen_id : int):
+	for kondimen_item in data_bumbu['kondimen']:
+		if kondimen_item['id'] == kondimen_id:
+			return kondimen_item
+	raise HTTPException(
+			status_code = 404,
+			detail = f'data tidak ditemukan'
+		)
+
+@app.get("/makanan/{food_id}")
+async def read_kondimen(food_id : int):
+	for food_item in data_menu['makanan']:
+		if food_item['id'] == food_id:
+			return food_item
+	raise HTTPException(
+			status_code = 404,
+			detail = f'data tidak ditemukan'
+		)
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8080)
